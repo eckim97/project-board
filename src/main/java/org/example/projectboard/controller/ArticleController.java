34b1870f849b0b46cb162b632eb6b69fd975e5f6
eckim97 <weoncheol1@naver.com>
2024-08-08 01:54:33 +0900

    #17 - 게시글 관련 뷰 테스트 작성
    우선 어떤 경로로 뷰를 열어줄지 테스트로 명세를 뽑는다.

---
+++ src/main/java/org/example/projectboard/controller/ArticleController.java
@@ -0,0 +1,9 @@
+package org.example.projectboard.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/articles")
+@Controller
+public class ArticleController {
+}
